3ffc3c631612fb9c4d8d48c079be68bb3c851f26
nmmilosevic <nm.milosevic@gmail.com>
2026-05-09 23:10:32 +0200

    ref

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,7 +3,13 @@ import { Geist } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
+import Script from "next/script";
 import "@/app/globals.css";
+
+const GTM_ID =
+  process.env.NEXT_PUBLIC_GTM_ID === ""
+    ? undefined
+    : (process.env.NEXT_PUBLIC_GTM_ID ?? "GTM-W7V3NLV5");
 
 const geistSans = Geist({
   subsets: ["latin"],
@@ -67,6 +73,28 @@ export default async function LocaleLayout({
         />
       </head>
       <body>
+        {GTM_ID ? (
+          <>
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                title="Google Tag Manager"
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              />
+            </noscript>
+            <Script id="google-tag-manager" strategy="afterInteractive">
+              {`
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');
+              `}
+            </Script>
+          </>
+        ) : null}
         <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
