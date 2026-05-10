35ebcde3c41252736ca65ce53885b0af3e4b09d5
nmmilosevic <nm.milosevic@gmail.com>
2026-05-10 10:15:30 +0200

    Gtag4

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,6 +3,7 @@ import { Geist } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
+import Script from "next/script";
 import "@/app/globals.css";
 
 const GTM_ID =
@@ -68,16 +69,13 @@ export default async function LocaleLayout({
         {GTM_ID ? (
           <>
             {/* Google Tag Manager */}
-            {/* eslint-disable-next-line @next/next/next-script-for-ga -- official inline <head> snippet from GTM */}
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            <Script id="google-tag-manager" strategy="afterInteractive">
+              {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`,
-              }}
-            />
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+            </Script>
             {/* End Google Tag Manager */}
           </>
         ) : null}
